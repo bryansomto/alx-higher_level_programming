4bd612bcf0c1e54a222538bc9aa0c7af5ba905fd
Bryan Somto <bryansomto@gmail.com>
2022-06-03 04:17:54 +0100

    first commit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the summ of 1 and 2."""
+    from add_0 import add
+    a = 1
+    b = 2
+    val = add(a, b)
+    print("{} + {} = {}".format(a, b, val))

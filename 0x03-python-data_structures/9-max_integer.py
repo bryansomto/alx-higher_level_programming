ac1477d37569536e4bb6001e5557d7f807310f83
Bryan Somto <bryansomto@gmail.com>
2022-06-05 13:51:21 +0100

    added task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    i = len(my_list) - 1
+    while i > 1:
+        j = 0
+        while j < i:
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = temp
+            j += 1
+        i -= 1
+    return my_list[-1]

483136631f83c4b29b42df12ac9e2208205b3cb1
Bryan Somto <bryansomto@gmail.com>
2022-06-05 12:22:36 +0100

    modified task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_list = list(my_string)
-    for letter in my_list:
-        if letter == 'c' or letter == 'C':
-            index = my_list.index(letter)
-            my_list.pop(index)
-    return("".join(my_list))
+    new_string = my_string.translate({ord(i): "" for i in 'cC'})
+    return new_string

5c3c7851c3232c47fefe3f456a431cdfde430fcd
Bryan Somto <bryansomto@gmail.com>
2022-07-06 03:48:15 +0100

    added tasks 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines an JSON-to-object function."""
+import json
+
+
+def to_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)

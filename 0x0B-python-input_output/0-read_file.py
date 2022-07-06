5341632a630e8243e9689c2c8e9eb925f8923a9a
Bryan Somto <bryansomto@gmail.com>
2022-07-06 03:15:30 +0100

    first commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="UTF-8") as a_file:
+        print(a_file.read(), end="")

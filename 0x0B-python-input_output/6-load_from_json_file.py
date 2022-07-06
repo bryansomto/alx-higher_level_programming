942722d225eb1aac8aca792bb77520ca6686a2ce
Bryan Somto <bryansomto@gmail.com>
2022-07-06 04:10:24 +0100

    added tasks

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a JSON file reading function."""
+"""Defines a JSON file-reading function."""
 import json
 
 
 def load_from_json_file(filename):
-    """Creates a Python object from a JSON file."""
-    with open(filename) as a_file:
-        json.load(a_file)
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)

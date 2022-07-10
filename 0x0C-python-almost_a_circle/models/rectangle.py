bc54b8f81514a4a191a87a4f3d70886d989ff0e3
Bryan Somto <bryansomto@gmail.com>
2022-07-10 11:51:40 +0100

    modified documentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -43,6 +43,7 @@ class Rectangle(Base):
         self.__y = value
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new rectangle method."""
         self.__width = width
         self.__height = height
         self.__x = x

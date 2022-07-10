6cb50b1c45557d8737afce8a72141d104e418e0b
Bryan Somto <bryansomto@gmail.com>
2022-07-10 10:06:19 +0100

    task 0 first commit

---
+++ 0x0C-python-almost_a_circle/tests/tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import unittest
+"""Defines a python test."""
+
+
+class TestProject(unittest.TestCase):
+    """Represent a test case."""
+
+    def test_samp(self):
+        """Test case 1."""
+        pass
+
+if __name__ == "__main__":
+    unittest.main()

37411d2bc70d2960d02068fbc309e1429d1c5700
Bryan Somto <bryansomto@gmail.com>
2022-07-12 23:51:29 +0100

    modified tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,107 +1,85 @@
 #!/usr/bin/python3
-
-"""Defines unittests for base.py.
-Unittest classes:
-    TestBase_instantiation - line 22
-    TestBase_to_json_string - line 110
-    TestBase_save_to_file - line 156
-    TestBase_from_json_string - line 234
-    TestBase_create - line 288
-    TestBase_load_from_file - line 340
-    TestBase_save_to_file_csv - line 406
-    TestBase_load_from_file_csv - line 484
-"""
-import os
-import unittest
+""" doctest unittest """
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
+import unittest
+import json
+import pep8
 
 
-class TestBase_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Base class."""
+class Testpep8(unittest.TestCase):
+    def test_pep8(self):
+        style = pep8.StyleGuide(quiet=True)
+        file_rectagle = "models/rectangle.py"
+        file_rectabgle_test = "tests/test_models/test_rectangle.py"
+        file_base = "models/base.py"
+        file_base_test = "tests/test_models/test_base.py"
+        file_square = "models/square.py"
+        file_square_test = "tests/test_models/test_square.py"
+        check = style.check_files([file_rectagle, file_rectabgle_test,
+                                  file_base, file_base_test, file_square,
+                                  file_square_test])
+        self.assertEqual(check.total_errors, 0,
+                         "Found code style errors (and warning).")
 
-    def test_no_arg(self):
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1)
 
-    def test_three_bases(self):
-        b1 = Base()
-        b2 = Base()
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 2)
+class TestBase(unittest.TestCase):
+    """ test """
 
-    def test_None_id(self):
-        b1 = Base(None)
-        b2 = Base(None)
-        self.assertEqual(b1.id, b2.id - 1)
+    @classmethod
+    def setUpClass(cls):
+        pass
 
-    def test_unique_id(self):
-        self.assertEqual(12, Base(12).id)
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
-    def test_nb_instances_after_unique_id(self):
-        b1 = Base()
-        b2 = Base(12)
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 1)
+    def setUp(self):
+        self.b1 = Base()
+        self.b2 = Base()
+        self.b3 = Base(-1)
+        self.b4 = Base(12)
+        self.b5 = Base("hola")
+        self.b6 = Base(3.1)
+        self.b7 = Base()
 
-    def test_id_public(self):
-        b = Base(12)
-        b.id = 15
-        self.assertEqual(15, b.id)
+    def tearDown(self):
+        pass
 
-    def test_nb_instances_private(self):
-        with self.assertRaises(AttributeError):
-            print(Base(12).__nb_instances)
+    def test_setid(self):
+        self.assertEqual(self.b1.id, 7)
 
-    def test_str_id(self):
-        self.assertEqual("hello", Base("hello").id)
+    def test_setid2(self):
+        self.assertEqual(self.b2.id, 11)
 
-    def test_float_id(self):
-        self.assertEqual(5.5, Base(5.5).id)
+    def test_setid3(self):
+        self.assertEqual(self.b3.id, -1)
 
-    def test_complex_id(self):
-        self.assertEqual(complex(5), Base(complex(5)).id)
+    def test_setid4(self):
+        self.assertEqual(self.b4.id, 12)
 
-    def test_dict_id(self):
-        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
+    def test_setid5(self):
+        self.assertEqual(self.b5.id, "hola")
 
-    def test_bool_id(self):
-        self.assertEqual(True, Base(True).id)
+    def test_setid6(self):
+        self.assertEqual(self.b6.id, 3.1)
 
-    def test_list_id(self):
-        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
+    def test_setid7(self):
+        self.assertEqual(self.b7.id, 27)
 
-    def test_tuple_id(self):
-        self.assertEqual((1, 2), Base((1, 2)).id)
+    def test_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(type(json_dictionary), str)
 
-    def test_set_id(self):
-        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
+    def test_json_string2(self):
+        r1 = Rectangle(10, 7, 2, 8, 1)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        list_back = json.loads(json_dictionary)
+        self.assertEqual(list_back, [dictionary])
 
-    def test_frozenset_id(self):
-        self.assertEqual(frozenset({1, 2, 3}), Base(frozenset({1, 2, 3})).id)
-
-    def test_range_id(self):
-        self.assertEqual(range(5), Base(range(5)).id)
-
-    def test_bytes_id(self):
-        self.assertEqual(b'Python', Base(b'Python').id)
-
-    def test_bytearray_id(self):
-        self.assertEqual(bytearray(b'abcefg'), Base(bytearray(b'abcefg')).id)
-
-    def test_memoryview_id(self):
-        self.assertEqual(memoryview(b'abcefg'), Base(memoryview(b'abcefg')).id)
-
-    def test_inf_id(self):
-        self.assertEqual(float('inf'), Base(float('inf')).id)
-
-    def test_NaN_id(self):
-        self.assertNotEqual(float('nan'), Base(float('nan')).id)
-
-    def test_two_args(self):
-        with self.assertRaises(TypeError):
-            Base(1, 2)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()

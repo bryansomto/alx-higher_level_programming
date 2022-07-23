db840d8091c9ab9d30137d7f5bf01ef23653a74b
Bryan Somto <bryansomto@gmail.com>
2022-07-23 05:16:58 +0100

    extra commits

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,5 +3,5 @@
 
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
+    """Return the dictionary representation of a simple data structure."""
     return obj.__dict__

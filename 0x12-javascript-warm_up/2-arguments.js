5b71a7e3399a888fd82d6c0538ae51616b48909a
Bryan Somto <bryansomto@gmail.com>
2022-09-05 19:18:04 +0100

    added task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-if (argv.length === 2) {
+if (argv.length === 1) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (argv.length === 2) {
   console.log('Argument found');
-} else if (argv.length > 3) {
+} else if (argv.length > 2) {
   console.log('Arguments found');
 }

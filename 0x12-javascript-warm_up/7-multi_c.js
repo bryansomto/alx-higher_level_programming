9acdc2e72120a6d3c59eab8b769eac7448f77266
Bryan Somto <bryansomto@gmail.com>
2022-09-06 05:07:44 +0100

    extra modifications

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 /* script that prints x times “C is fun” */
-let arg = Math.floor(Number(process.argv[2]));
-if (isNaN(arg)) {
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  while (arg) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
-    arg--;
   }
-}
+} 

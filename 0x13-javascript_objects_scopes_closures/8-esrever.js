f94f2bc24cb7a5b91806bb62f56d22e246c421e2
Bryan Somto <bryansomto@gmail.com>
2022-09-07 01:42:23 +0100

    code fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 /* a function that returns the reversed version of a list */
 exports.esrever = function (list) {
-  const tempList = [];
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
+  /* const tempList = [];
   let len = 0;
   for (let i = 1; i < list.length; i++) {
     len += 1;
@@ -9,5 +13,5 @@ exports.esrever = function (list) {
   for (let j = len; j >= 0; j--) {
     tempList.push(list[j]);
   }
-  return (tempList);
+  return (tempList); */
 };

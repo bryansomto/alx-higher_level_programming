73877bbbf45b879061253491d4c3b8306bd20b8a
bryansomto <bryansomto@gmail.com>
2022-09-10 21:51:01 -0700

    added task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# List state database hbtn_0e_0_usa
+# Syntax: ./0-select_states.py username password database_name
+import sys
+import mySQLdb
+
+if __name__ = "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    [print(state) for state in cur.fetchall()]

5a1748f6b345849fddc266d1aa35094984d267b5
bryansomto <bryansomto@gmail.com>
2022-09-10 22:13:05 -0700

    fixed .py errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,9 @@
 # List state database hbtn_0e_0_usa
 # Syntax: ./0-select_states.py username password database_name
 import sys
-import mySQLdb
+import MySQLdb
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")

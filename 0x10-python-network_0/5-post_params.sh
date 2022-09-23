0c5a37ceace24f23b7ec73a7dc56fb6ff183e344
Bryan Somto <bryansomto@gmail.com>
2022-09-23 02:52:34 +0100

    modified task

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a POST request and displays the body response
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"

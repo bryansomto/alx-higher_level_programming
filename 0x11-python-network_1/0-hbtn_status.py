7d2d0f61da1f01453070892feee01fe85288ea13
Bryan Somto <bryansomto@gmail.com>
2022-09-23 23:31:40 +0100

    added task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a Python script that fetches https://alx-intranet.hbtn.io/status """
+
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))

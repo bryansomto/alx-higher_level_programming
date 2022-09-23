3d807d9b369439856297fbd274c97d81b66e41cc
Bryan Somto <bryansomto@gmail.com>
2022-09-23 23:50:47 +0100

    modified task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """a Python script that:
 - takes in a URL,
-- sends a request to the URL 
-- and displays the value of the X-Request-Id variable found in the header of the response.
+- sends a request to the URL
+- and displays the value of the X-Request-Id variable
+    found in the header of the response.
 """
 import sys
 import urllib.request

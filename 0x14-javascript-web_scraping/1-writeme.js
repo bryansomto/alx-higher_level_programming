15116e55c529291db1c318e22453a2440c20bddd
bryansomto <bryansomto@gmail.com>
2022-10-18 19:27:45 +0100

    added task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* A script that writes a string to a file. */
+const fs = require('fs');
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+fs.writeFile(arg1, arg2, 'utf-8', function (error) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('arg2 > arg1');
+  }
+});

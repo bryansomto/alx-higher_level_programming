e0248f4f83782113b844c56fd24fa28f247a5ccf
bryansomto <bryansomto@gmail.com>
2022-10-18 19:14:37 +0100

    extra modifications

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* A script that reads and prints the content of a file. */
 const fs = require('fs');
-const arg = process.argv[1];
+const arg = process.argv[2];
 fs.readFile(arg, 'utf-8', function (error, content) {
   if (error) {
     console.log(error);
